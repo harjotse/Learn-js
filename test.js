6fc1eadd16e9b5feb5bf695a8938730c1ec87416
Harjot Singh <bhatiaharjot2000@gmail.com>
2026-09-15 22:04:06 +0530

    commit

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("harjot")
+
+// varible 
+const accountID = 2342
+let accountEmail= "harjot@gmail.com"
+var accountPassword= "23423"
+accountCity="mohali"
+// const accountID =2342 // not allowed  
+console.log(accountId) // 
