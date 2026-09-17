fb6803aeb939a9dbe161f06c8f7bfc8a4c9259b4
Harjot Singh <bhatiaharjot2000@gmail.com>
2026-09-17 22:28:36 +0530

    commit

--- dataTypes.js
+++ dataTypes.js
@@ -2,4 +2,14 @@
  let age =188
  let isLoggedIn=false
  let state=null
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+ //int 
+ //bigint
+ //string
+ //boolean
 // null is nothing empty and undefined is like the value is not assigned 
+//symbol=>for uniqueness 
+//object  
