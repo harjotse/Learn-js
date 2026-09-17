d9d6afb204a51ea2ee6897e8c589029932d23be1
Harjot Singh <bhatiaharjot2000@gmail.com>
2026-09-17 22:14:54 +0530

    commit

--- dataTypes.js
+++ dataTypes.js
@@ -1,1 +1,5 @@
 " use strict";// treat all js code as newer version 
+ let age =188
+ let isLoggedIn=false
+ let state=null
+// null is nothing empty and undefined is like the value is not assigned 
