cf1237305b2ee705568bbb26942aca54b79ee2c7
Harjot Singh <bhatiaharjot2000@gmail.com>
2026-09-21 23:07:33 +0530

    commit

---
+++ comparisons.js
@@ -0,0 +1,18 @@
+console.log(1==1)
+console.log(1>2)
+console.log(1>=122)
+console.log(1!=12)
+// simple conversions
+
+
+// conversions to avoid coz they give mix ans this is js fault due to no strict type checking
+console.log("2">1)
+console.log(2=="1")
+console.log(2>="1")
+console.log(null>="1")
+console.log(null>="1")
+console.log(null>="1")
+console.log(undefined >="1")
+console.log(undefined>="1")
+console.log(undefined>="1")
+
